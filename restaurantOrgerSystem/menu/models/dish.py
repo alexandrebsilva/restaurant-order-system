e4b82c42428ca82eb825bb339de9c104f4136126
Alexandre Borges <alexandre.bsilva55@gmail.com>
2024-11-18 08:41:30 -0300

    first commit

---
+++ restaurantOrgerSystem/menu/models/dish.py
@@ -0,0 +1,14 @@
+from django.db import models
+from .menu import Menu
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='dishes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
